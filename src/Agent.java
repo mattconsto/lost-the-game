c035f9e9cbbd3b3adf38f4fe84f0ab5c04595fa1
steppers <stepz69@gmail.com>
2015-01-23 22:05:53 +0000

    Reset Everything!

---
+++ src/Agent.java
@@ -0,0 +1,5 @@
+
+public class Agent {
+	public int hunger;
+	public int thirst;
+}
---
+++ src/Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+	protected String name;
+	
+	public Game(String name) {
+		this.name = name;
+	}
+	
+	public void draw() {
+		System.out.println(name);
+	}
+}
