bc804b80d9fb1cb92be7e4dd3ad604639254f18a
steppers <stepz69@gmail.com>
2015-01-23 23:13:00 +0000

    Added the tile system base

---
+++ src/TileSystem/Camera.java
@@ -0,0 +1,20 @@
+package TileSystem;
+
+public class Camera {
+
+	private float x, y;
+	
+	public Camera(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX(){
+		return x;
+	}
+	
+	public float getY(){
+		return y;
+	}
+	
+}
