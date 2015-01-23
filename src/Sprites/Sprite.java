f3e62dd6934ccd1c35b4c59699930581ea7d7059
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-01-23 23:56:34 +0000

    Added skeleton sprite

---
+++ src/Sprites/Sprite.java
@@ -0,0 +1,14 @@
+package Sprites;
+
+import TileSystem.TileSystem;
+import org.newdawn.slick.Image;
+
+/**
+ * Assuming 32x32 tiles
+ * Created by andy on 23/01/15.
+ */
+public class Sprite {
+    public Image getSpriteByTileId(TileSystem.TileId tileId) {
+        return null;
+    }
+}
