0a37a14dfb1290e23a310627875d5a84758565dd
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-01-24 12:14:24 +0000

    Tilesystem now has all variants

--- src/Sprite/GroundSprite.java
+++ src/Sprite/GroundSprite.java
@@ -11,6 +11,17 @@ import java.util.HashMap;
  * Created by andy on 24/01/15.
  */
 public class GroundSprite {
+    /* TILES WE CAN'T HAVE
+    * -------------------
+    * 1-wide Grass against Dirt,
+    * 1-wide Dirt against Water,
+    * Grass with >1 corners and 0 sides of Dirt,
+    * Dirt with >1 corners and 0 sides of Water,
+    * Grass surrounded by > 2 sides of Dirt
+    * Dirt surrounded by > 2 sides of Water
+    * */
+
+
 
     //returns the location of the tile in grid
     public static Point getSprite(TileSystem.TileId tileId, int variant) {
@@ -29,48 +40,67 @@ public class GroundSprite {
         }
     }
 
-
-
     public static HashMap<TileSystem.TileId, ArrayList<Point>> tileToLocation = new HashMap<TileSystem.TileId, ArrayList<Point>>(){{
+        // Water is the most important, so it only has 1 variation.
+        put(TileSystem.TileId.WATER, new ArrayList<Point>() {{
+            add(new Point(1,14));// ┼
+        }});
+        put(TileSystem.TileId.DIRT, new ArrayList<Point>() {{
+            add(new Point(5,14));// ┼
+            // Corner Adjacent Water
+            add(new Point(4+1 ,4+1 ));// ┌
+            add(new Point(4+2 ,4+1 ));// ┐
+            add(new Point(4+1 ,4+2 ));// └
+            add(new Point(4+2 ,4+2 ));// ┘
+            // 1 Side adjacent Water
+            add(new Point(4+1 ,4+0 ));// ┬
+            add(new Point(4+0 ,4+1 ));// ├
+            add(new Point(4+3 ,4+1 ));// ┤
+            add(new Point(4+1 ,4+3 ));// ┴
+            // 2 Side adjacent Water
+            add(new Point(4+0 ,4+0 ));// ┌
+            add(new Point(4+3 ,4+0 ));// ┐
+            add(new Point(4+0 ,4+3 ));// └
+            add(new Point(4+3 ,4+3 ));// ┘
+        }});
         put(TileSystem.TileId.GRASS, new ArrayList<Point>() {{
-            add(new Point(15,1 ));// ┼
-        }});
-        put(TileSystem.TileId.GRASSWATER, new ArrayList<Point>() {{
-            add(new Point(0 ,0 ));// ┌
+            add(new Point(1,15));// ┼
+
+            // Grass -> Water
+            // Corner Adjacent Water
+            add(new Point(1 ,1 ));// ┌
+            add(new Point(2 ,1 ));// ┐
+            add(new Point(1 ,2 ));// └
+            add(new Point(2 ,2 ));// ┘
+            // 1 Side adjacent Water
             add(new Point(1 ,0 ));// ┬
-            add(new Point(3 ,0 ));// ┐
             add(new Point(0 ,1 ));// ├
-            add(new Point(15,1 ));// ┼
             add(new Point(3 ,1 ));// ┤
-            add(new Point(0 ,3 ));// └
             add(new Point(1 ,3 ));// ┴
+            // 2 Side adjacent Water
+            add(new Point(0 ,0 ));// ┌
+            add(new Point(3 ,0 ));// ┐
+            add(new Point(0 ,3 ));// └
             add(new Point(3 ,3 ));// ┘
-        }});
-        put(TileSystem.TileId.GRASSDIRT, new ArrayList<Point>() {{
-            // To Grass
-            add(new Point(1 ,1+4 ));// ┌
-            add(new Point(1 ,0+4 ));// ┬
-            add(new Point(3 ,0+4 ));// ┐
-            add(new Point(0 ,1+4 ));// ├
-            add(new Point(15,1+4 ));// ┼
-            add(new Point(3 ,1+4 ));// ┤
-            add(new Point(0 ,3+4 ));// └
-            add(new Point(1 ,1+4 ));// ┴
-            add(new Point(1 ,1+4 ));// ┘
 
-        }});
-
-        put(TileSystem.TileId.DIRTWATER, new ArrayList<Point>() {{
-            // To Grass
-            add(new Point(1 ,1+4 ));// ┌
-            add(new Point(1 ,0+4 ));// ┬
-            add(new Point(3 ,0+4 ));// ┐
-            add(new Point(0 ,1+4 ));// ├
-            add(new Point(15,1+4 ));// ┼
-            add(new Point(3 ,1+4 ));// ┤
-            add(new Point(0 ,3+4 ));// └
-            add(new Point(1 ,1+4 ));// ┴
-            add(new Point(1 ,1+4 ));// ┘
+            // Grass -> Dirt
+            // Corner Adjacent Dirt
+            add(new Point(1 ,4+1 ));// ┌
+            add(new Point(2 ,4+1 ));// ┐
+            add(new Point(1 ,4+2 ));// └
+            add(new Point(2 ,4+2 ));// ┘
+            // 1 Side Adjacent Dirt
+            add(new Point(1 ,4+0 ));// ┬
+            add(new Point(3 ,4+1 ));// ├
+            add(new Point(0 ,4+1 ));// ┤
+            add(new Point(1 ,4+3 ));// ┴
+            // 2 Side Adjacent Dirt
+            add(new Point(0 ,4+0 ));// ┌
+            add(new Point(3 ,4+0 ));// ┐
+            add(new Point(0 ,4+3 ));// └
+            add(new Point(3 ,4+3 ));// ┘
+            // Dirt on either side is Illegal
+            // 3 Side Adjacent Dirt is Illegal
 
         }});
     }};
