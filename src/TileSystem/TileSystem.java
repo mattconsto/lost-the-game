ed8eee99bd5a60930da94979133bb645604eb354
steppers <stepz69@gmail.com>
2015-01-24 16:54:33 +0000

    Fixed Fog out of bounds

--- src/TileSystem/TileSystem.java
+++ src/TileSystem/TileSystem.java
@@ -166,9 +166,10 @@ public class TileSystem {
 			int yp = (int)p.location.y;
 			for(int x = xp - 5; x < xp + 5; x++){
 				for(int y = yp - 5; y < yp + 5; y++){
-					if(dist(xp, yp, x, y) <= 4){
-						tiles[x][y].vis = 2;
-					}
+					if(x > 0 && y > 0 && x < size && y < size)
+						if(dist(xp, yp, x, y) <= 4){
+							tiles[x][y].vis = 2;
+						}
 				}
 			}
 		}
