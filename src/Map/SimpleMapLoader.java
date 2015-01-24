233eb9dd9acd8a6dfc20a0b8e31bdb01ec27ca89
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-01-24 15:43:50 +0000

    Added simple map loader (not working yet)

---
+++ src/Map/SimpleMapLoader.java
@@ -0,0 +1,61 @@
+package Map;
+
+import TileSystem.TileSystem;
+
+import java.io.*;
+
+/**
+ * Created by andy on 24/01/15.
+ */
+public class SimpleMapLoader {
+    private static final String filePath = "map.txt";
+
+    public TileSystem.Tile[][] loadMap() throws FileNotFoundException {
+        BufferedReader reader = null;
+        try {
+            File file = new File(filePath);
+            reader = new BufferedReader(new FileReader(file));
+
+            // First line is the size (Assuming it's a square)
+            int size = Integer.parseInt(reader.readLine());
+            if (size < 1) {
+                throw new Exception("Map has invalid grid size!");
+            }
+
+            TileSystem.Tile[][] tiles = new TileSystem.Tile[size][size];
+
+            // Iterate through the file to set the map up.
+            for (int x = 0; x < size; x++) {
+                String line = reader.readLine();
+                for (int y = 0; y < line.length(); y++) {
+                    createTile(line.charAt(y), x, y);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                //ohgodwhathashappened?
+            }
+        }
+    }
+
+    private TileSystem.Tile createTile(char mapChar, int x, int y) {
+        switch(mapChar) {
+            case 'M':
+                return null;//new TileSystem.Tile(TileSystem.TileId.WATER,x,y,2);
+            default:
+                return null;//new TileSystem.Tile(TileSystem.TileId.DIRT,x,y,2);
+        }
+    }
+
+}
