a65c6bcbdca6b5e06cc60d28142df420aca71b8c
steppers <stepz69@gmail.com>
2015-01-25 07:14:58 +0000

    Fire Should now produce a fog free area

--- src/TileSystem/TileSystem.java
+++ src/TileSystem/TileSystem.java
@@ -221,6 +221,21 @@ public class TileSystem {
 				}
 			}
 		}
+		
+		for(int x = 0; x < size; x++){
+            for(int y = 0; y < size; y++){
+            	if(tiles[x][y].attr == TileAttr.FIRE){
+            		for(int xp = x - 10; xp < x + 10; xp++){
+						for(int yp = y - 10; yp < y + 10; yp++){
+							if(xp > 0 && yp > 0 && xp < size && yp < size)
+								if(dist(xp, yp, x, y) <= 9){
+									tiles[x][y].vis = 2;
+								}
+						}
+					}
+            	}
+            }
+		}
 	}
 	
 	private float dist(int x, int y, int x2, int y2){
