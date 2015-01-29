0ad564f92b5007c0af1c5d4042cee83396b45700
Oliver Steptoe <therealmgames@gmail.com>
2015-01-29 12:49:06 +0000

    h

--- src/Graphics/Renderer.java
+++ src/Graphics/Renderer.java
@@ -1,7 +1,40 @@
 package Graphics;
 
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+
 public class Renderer {
 	
+	private int maxSprites;
 	
+	private float[] batch;
+	private int i = 0;
+	
+	public Renderer(int maxSprites){
+		this.maxSprites = maxSprites;
+		batch = new float[maxSprites*8];
+	}
+	
+	//Creates a new batch
+	public void begin(Texture tex){
+		tex.bind();
+		i = 0;
+	}
+	
+	public void drawImage(float x, float y, float width, float height, int srcX, int srcY, int srcX2, int srcY2){
+		//Top left
+		batch[i++] = x;
+		batch[i++] = y;
+		batch[i++] = srcX;
+		batch[i++] = srcY;
+		batch[i++] = x;
+		batch[i++] = y;
+	}
+	
+	//Ends the current batch and sends it to the gpu
+	public void end(){
+		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(currentBatch.length);
+	}
 
 }
